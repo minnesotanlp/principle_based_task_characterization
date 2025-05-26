36834266c9f43f1dfe1b3bc8650d1ab3b40bd157
Ether9t <wan01492@umn.edu>
2025-05-26 15:46:17 -0500

    Init skeleton

---
+++ src/routes/+page.svelte
@@ -0,0 +1,91 @@
+<script>
+
+</script>
+
+<div>
+    <h1 class="publication_title">
+        From Rubrics to Recipe: Principle-Centric Benchmarking and Characterization of Tasks for Language Models
+    </h1>
+    <div class="pulication_authors">
+        <span>
+            <a href="https://www.shirley.id/" target="_blank"
+            >Shirley Anugrah Hayati</a
+            ><sup></sup>,
+        </span>
+        <span>
+            <a href="https://ether9t.github.io" target="_blank"
+            >Ruizi Wang</a
+            ><sup></sup>,
+        </span>
+        <span>
+            <a href="https://dykang.github.io/" target="_blank"
+            >Dongyeop Kang</a
+            ><sup></sup>,
+        </span>
+    </div>
+</div>
+<div>
+    <h2 class="paper_title">
+        Abstract
+    </h2>
+    <p class="paper_detail">
+        As large language models (LLMs) are increasingly applied to a wide range of NLP tasks, it becomes important to systematically understand what defines these tasks beyond dataset labels or surface instructions. 
+        In this work, we explore whether tasks can be characterized using principles. Previous studies have mainly focused on coarse-grained principles and saying that principles apply to all tasks. Others propose principles that are very specific to instances. 
+        Our study balances the general and specific principles. We introduce a framework for automatically annotating tasks with such principles and investigate their utility for organizing task collections and diagnosing model behavior. We also introduce a new principle-guided benchmarking dataset containing more than 20k instances. 
+        Our findings shows that principles can effectively guide LLMs in generating data that meets expected standards. Dataset and code will be released upon acceptance.
+    </p>
+</div>
+<div>
+    <h2 class="paper_title">
+        Research Contributions
+    </h2>
+</div>
+<div>
+    <h2 class="paper_title">
+        Methods
+    </h2>
+</div>
+<div>
+    <h2 class="paper_title">
+        Key Takeaways
+    </h2>
+</div>
+    
+<style>
+  * {
+    font-family: 'Poppins', sans-serif;
+  }
+
+  .publication_title {
+    text-align: center;
+    font-size: 36px; 
+    margin-top: 50px; 
+    margin-left: 50px; 
+    margin-right: 100px;
+    color: #363636;
+  }
+  
+  .pulication_authors {
+    text-align: center;
+    font-size: 18px; 
+  }
+
+  .paper_title {
+    text-align: center;
+    font-size: 24px; 
+    color: #363636;
+  }
+
+  .paper_detail {
+    font-size: 16px; 
+    text-align: justify;
+    margin-left: 300px;
+    margin-right: 300px;
+    color: #4a4a4a;
+  }
+</style>
+
+<svelte:head>
+  <title>From Rubrics to Recipe: Principle-Centric Benchmarking and Characterization of Tasks for Language Models</title>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
+</svelte:head>
