82e39aeb4b3ff69c3d2e15c99439355d8e7cc349
Ether9t <wan01492@umn.edu>
2025-05-28 13:41:44 -0500

    Add figure and table

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,23 @@
 <script>
+  import { onMount } from 'svelte';
+  import 'bulma-carousel/dist/css/bulma-carousel.min.css';
 
+  let carousel;
+
+  onMount(async () => {
+    const module = await import('bulma-carousel');
+    const bulmaCarousel = module.default;
+
+    const carousels = bulmaCarousel.attach('#carousel', {
+      slidesToScroll: 1,
+      slidesToShow: 1,
+      navigation: true,
+      pagination: true,
+      loop: true
+    });
+
+    carousel = carousels[0];
+  });
 </script>
 
 <div>
@@ -34,6 +52,13 @@
         In this work, we explore whether tasks can be characterized using principles. Previous studies have mainly focused on coarse-grained principles and saying that principles apply to all tasks. Others propose principles that are very specific to instances. 
         Our study balances the general and specific principles. We introduce a framework for automatically annotating tasks with such principles and investigate their utility for organizing task collections and diagnosing model behavior. We also introduce a new principle-guided benchmarking dataset containing more than 20k instances. 
         Our findings shows that principles can effectively guide LLMs in generating data that meets expected standards. Dataset and code will be released upon acceptance.
+    </p>
+    <div style="display: flex; justify-content: center">
+        <img src="main_figure_paper.jpg" style="width: 25%" alt="main_figure_paper"/>
+    </div>
+    <p class="paper_detail" style="text-align: center; color: #363636; font-size: 12px;">Figure 1: Some principles are specific to that task. For a travel plan task, a user who asks LLM to design a travel schedule (input), require an output that adheres to certain principles.
+        <strong>efficient planning</strong> and <strong>photography</strong> may be essential while <strong>fine dining</strong> is irrelevant unless explicitly asked. 
+        Our framework extract these principles from human rubrics and can also ask an LLM to propose additional principles, producing dynamic, task-aware evaluations.
     </p>
 </div>
 
@@ -82,40 +107,35 @@
     </div>
     <p class="paper_detail" style="text-align: center; color: #363636; font-size: 12px;">Figure 2: LLM extracts principles from a given task, and then generates instances (input-output pairs) given the principles, and finally uses the principles to evaluate models' responses.</p>
     <div class="paper_detail">
-        <p>
-            To obtain task-level principles, we have two approaches.
-        </p>
         <ul>
             <li>
                 Step 1: Principle Generation
                     <p>
-                        We use two approaches to obtain task-level principles. In the first approach, we prompt an LLM with an input, an output, criteria, and a rubric score of 5 from BiGGen Bench to extract principles. We refer to these as <strong>extracted principles</strong>.
+                        <strong>Extracted principles</strong> = we prompt an LLM with an input, an output, criteria, and a rubric score of 5 from BiGGen Bench to extract principles, and then cluster similar principles and ask the LLM to choose a representative name and summary. 
+                        On average, 13.1 principles are extracted per task, resulting in a total of 917.
                     </p>
                     <p>
-                        Since some criteria within the same task are similar, the LLM may generate overlapping principles. We cluster similar principles and ask the LLM to choose a representative name and summary. On average, 13.1 principles are extracted per task, resulting in a total of 917.
-                    </p>
-                    <p>
-                        In the second approach, we generate principles using one-shot prompting, providing only the task name without examples. We refer to these as <strong>generated principles</strong>. On average, 16.85 principles are generated per task, with a total of 1,180.
+                        <strong>Generated principles</strong> = we generate principles using one-shot prompting, providing only the task name without examples. On average, 16.85 principles are generated per task, with a total of 1,180.
                     </p>
             </li>
             <li>
                 Step 2: Instance Generation
             </li>
             <p>
-                We test if the LLM follows principles by prompting it with one specific principle to generate 10 instances each, totaling 20,970 instances.
+                <strong>With principles</strong> = we prompt the LLM with a specific principle and ask it to generate 10 instances that adhere to it. This results in a total of 20,970 instances.
             </p>
             <p>
-                As a baseline, we generate 100 instances per task without using principles, via iterative bootstrapping.
+                <strong>Without principles (baseline)</strong> = we use an iterative bootstrapping method without any principles, generating 100 instances per task.
             </p>
             <p>
-                For both principle-based and baseline generation, we provide 10 BiGGen Bench examples to guide the format, not the content.
+                In both settings, 10 example instances from BiGGen Bench are shown to guide format, not content.
             </p>
             <li>
                 Step 3: Model Inference and Evaluation
             </li>
             <p>
-                To examine how much the generated data instances follow the principles, we ask GPT-4o to evaluate the quality of each response based on the principles. 
-                GPT-4o must give a score from 0 to 5, defined as follows:
+                We use GPT-4o to assess how well each generated response adheres to the given principles. 
+                For each principle, GPT-4o assigns a score from 0 to 5:
             </p>
             <ul>
                 <li>
@@ -150,6 +170,12 @@
             <li>
                 What principles characterize a task?
             </li>
+            <div style="display: flex; justify-content: center">
+                <img src="cooccurrence.jpg" style="width: 75%" alt="cooccurrence"/>
+            </div>
+            <p style="text-align: center; color: #363636; font-size: 12px;">
+                Figure 3: Principle co-occurrences for three tasks (travel plan, moral belief, and math proof) from different model capabilities. Top graphs are principles on data from BigGen Bench while bottom graphs are principles on dataset generated by LLM without principle guidance.
+            </p>
             <p>
                 We analyzed principle co-occurrences in human-curated and LLM-generated instances without explicit principles. <br>
                 By inferring and selecting high-scoring principles from the latter, we observed clear differences in usage and co-occurrence patterns. <br>
@@ -158,13 +184,25 @@
             <li>
                 Similarity between LLM-extracted and generated principles vs. human-written Principles?
             </li>
+            <div style="display: flex; justify-content: center">
+                <img src="principle_similarity.png" style="width: 125%" alt="principle_similarity"/>
+            </div>
+            <p style="text-align: center; color: #363636; font-size: 12px;">
+                Table 1: Cosine similarity score between human-written principles, LLM-extracted principles (Extract), and LLM-generated principles (Generate).
+            </p>
             <p>
                 We compare LLM-extracted and generated principles to human-written ones created from task names and descriptions. <br>
                 Using SentenceBERT embeddings, both LLM-generated and extracted principles show similar similarity to human-written principles, with average cosine scores of 0.37 and 0.35, respectively.
             </p>
             <li>
-                How useful are principles for data generation
+                How useful are principles for data generation?
             </li>
+            <div style="display: flex; justify-content: center">
+                <img src="travel_plan.png" style="width: 125%" alt="travel_plan"/>
+            </div>
+            <p style="text-align: center; color: #363636; font-size: 12px;">
+                Table 2: Comparison of Extracted and Generated Principles Data for task Travel Plan.
+            </p>
             <p>
                 We evaluate the impact of providing principles during data generation using a rubric. 
                 Generation with principles consistently achieves higher adherence, especially for unfamiliar principles. <br> 
@@ -174,6 +212,26 @@
             <li>
                 Benchmarking LLMs
             </li>
+            <div id="carousel" class="carousel results-carousel">
+                <div class="item">
+                    <div style="display: flex; justify-content: center; margin-top: 125px">
+                        <img src="eval_results_subcolumns.png" style="width: 100%;" alt="eval_results_subcolumns" />
+                    </div>
+                    <p>Table 3: Evaluation results on generated principles (Gen.) and extracted (Extr.) principles for various LLMs. <strong>Bold</strong> numbers are the highest scores for a capability for generated principles while <u>underlined</u> numbers are highest scores for extracted principles.</p>
+                </div>
+                <div class="item">
+                    <div style="display: flex; justify-content: center">
+                        <img src="reasoning.jpg" style="width: 45%;" alt="reasoning" />
+                    </div>
+                    <p>Figure 4: Reasoning</p>
+                </div>
+                <div class="item">
+                    <div style="display: flex; justify-content: center">
+                        <img src="safety.jpg" style="width: 45%;" alt="safety" />
+                    </div>
+                    <p>Figure 5: Safety</p>
+                </div>
+            </div>
             <p>
                 We propose a principle-based benchmarking approach that evaluates LLMs on specific qualities such as planning, reasoning, and cultural awareness, offering more precise insights than general benchmarks. <br>
                 Using this method, we assessed variants of Gemma-2 and Qwen 2.5, with GPT-4 as a reference. 
@@ -217,6 +275,21 @@
     margin-left: 300px;
     margin-right: 300px;
     color: #4a4a4a;
+  }
+
+  .carousel {
+    max-width: 100%;
+    overflow: hidden;
+    position: relative;
+  }
+
+  .carousel .item {
+    text-align: center;
+  }
+
+  .carousel .item img {
+    display: block;
+    margin: 0 auto;
   }
 </style>
 
